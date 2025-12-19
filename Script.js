b87e3ff1a5e2464084b0736a035a94b1f6b630f8
ChathukaHettiArachchi <161861880+ChathukaHettiArachchi@users.noreply.github.com>
2025-12-19 16:01:31 +0530

    Separate Script file

--- Script.js
+++ Script.js
@@ -1,2 +1,6 @@
-// Placeholder for future features
-console.log("Clothify Store Loaded");
+ const btn = document.getElementById("menuBtn");
+        const menu = document.getElementById("mobileMenu");
+
+        btn.addEventListener("click", () => {
+            menu.classList.toggle("hidden");
+        });
